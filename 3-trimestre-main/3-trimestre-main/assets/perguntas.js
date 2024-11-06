a2dd07dc97b9bd6bd59a6318b5902507295a99dd
MizinhaVidaLoka244 <160685207+MizinhaVidaLoka244@users.noreply.github.com>
2024-11-05 21:57:35 -0300

    Add files via upload

---
+++ 3-trimestre-main/3-trimestre-main/assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+     'Programação',
+     perguntas,
+     resposta
+)
+
+criaCartao(
+    categoria,
+    perguntas,
+    resposta
+  )
+
+  criaCartao(
+    categoria,
+    perguntas,
+    resposta
+)
